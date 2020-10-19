49477c60fbc0678c8173cdeb5165adfa7e61355f
KiranAcharya <kiranacharya287@gmail.com>
2020-10-19 14:38:11 +0530

    Updates

---
+++ precache-manifest.e35c49d8c4d3b68d316d2e17aacb7a2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386c9839b5a3f2887bcfe93e4a4b759b",
+    "url": "/GitHub-users-React/index.html"
+  },
+  {
+    "revision": "7564cda1fba1957bd319",
+    "url": "/GitHub-users-React/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "16a6ee6166c1f22a02ef",
+    "url": "/GitHub-users-React/static/css/main.4dd33b89.chunk.css"
+  },
+  {
+    "revision": "7564cda1fba1957bd319",
+    "url": "/GitHub-users-React/static/js/2.23b0a45d.chunk.js"
+  },
+  {
+    "revision": "7f01fb7a78c6328cf95a6a5991494193",
+    "url": "/GitHub-users-React/static/js/2.23b0a45d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a6ee6166c1f22a02ef",
+    "url": "/GitHub-users-React/static/js/main.60a4beb4.chunk.js"
+  },
+  {
+    "revision": "4f8a9222b35a990608b0",
+    "url": "/GitHub-users-React/static/js/runtime-main.ee4756d5.js"
+  }
+]);
